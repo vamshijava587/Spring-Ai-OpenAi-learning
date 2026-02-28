3f27a5bb9ceff9677d8ef6df51dc1a9fe08d975c
vamshijava587 <vamshijavaspringboot@gmail.com>
2026-02-28 13:13:42 +0530

    updated prompts

--- src/main/java/com/vamshi/springopenai/SpringAiOpenAiApplication.java
+++ src/main/java/com/vamshi/springopenai/SpringAiOpenAiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling  // Add this
+@EnableScheduling
 public class SpringAiOpenAiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAiOpenAiApplication.class, args);
